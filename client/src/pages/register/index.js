dda68c7923826711b0badc9ccd41f893acfe6089
jungerion <jungerion@gmail.com>
2023-08-27 20:57:40 +0545

    register/index page error handled/ after client resetup

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
+// import styles from "../../styles/register.module.css";
 import styles from "../../styles/register.module.css";
 
 const SignupSchema = Yup.object().shape({
@@ -12,10 +13,18 @@ const SignupSchema = Yup.object().shape({
     .min(2, "Too Short!")
     .max(50, "Too Long!")
     .required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
+  // email: Yup.string().email("Invalid email").required("Required"),
 });
 
 const Register = () => {
+  const handleRegister = (values) => {
+    // debugger;
+    fetch("http://localhost:3005/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    });
+  };
   return (
     <div className="styles.form">
       <h1>Signup</h1>
@@ -24,10 +33,12 @@ const Register = () => {
           fullName: "",
           phoneNumber: "",
           password: "",
+          confirmPassword: "",
         }}
         validationSchema={SignupSchema}
         onSubmit={(values) => {
           // same shape as initial values
+          handleRegister(values);
           console.log(values);
         }}
       >
@@ -46,6 +57,15 @@ const Register = () => {
             <Field name="password" type="password" placeholder="password" />
             {errors.password && touched.password ? (
               <div>{errors.password}</div>
+            ) : null}{" "}
+            <br />
+            <Field
+              name="confirmPassword"
+              type="confirmPassword"
+              placeholder="confirmPassword"
+            />
+            {errors.confirmPassword && touched.confirmPassword ? (
+              <div>{errors.confirmPassword}</div>
             ) : null}{" "}
             <br />
             <button type="submit">Submit</button>
